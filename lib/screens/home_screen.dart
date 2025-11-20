b8de960b8d867b0311817dc08688458ff342aba5
Nakul T <nakulunni93@gmail.com>
2025-11-20 16:22:49 +0530

    refactor: enhance HomeScreen layout with search bar, notifications, and improved item display

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,43 +8,150 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hello bro'),
+        actions: [
+          TextButton(onPressed: () {}, child: Text('azhikode')),
+        ],
       ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(hintText: 'Searc karo'),
+      body: CustomScrollView(
+        slivers: [
+          // Search bar + notifications
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.search),
+                        hintText: "Search karo",
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  IconButton(
+                    icon: Icon(Icons.notifications),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
           ),
-          SizedBox(
-            height: 40,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-                  width: 30,
-                  height: 30,
-                  color: Colors.amber,
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Container(
-                  width: 30,
-                  height: 30,
-                  color: Colors.amber,
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Container(
-                  width: 30,
-                  height: 30,
-                  color: Colors.amber,
-                ),
-              ],
+
+          // Category list
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 40,
+              child: CaategoryList(),
+            ),
+          ),
+
+          // Title text
+          SliverPadding(
+            padding: EdgeInsets.all(15),
+            sliver: SliverToBoxAdapter(
+              child: Text(
+                "Recommendation",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+          // Infinite Scrollable Grid
+          SliverPadding(
+            padding: EdgeInsets.all(15),
+            sliver: SliverGrid.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 3 / 4,
+              children: List.generate(20, (index) {
+                return ItemWidget();
+              }),
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey[200],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(alignment: AlignmentGeometry.topRight, child: FavoriteButton()),
+          Center(child: Image.asset('assets/images/logo.png', width: 100)),
+          SizedBox(height: 10),
+
+          Text("₹"),
+          Text("name"),
+        ],
+      ),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({
+    super.key,
+  });
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.favorite),
+      color: isFavorite ? Colors.red : Colors.grey,
+      iconSize: 16,
+      // padding: EdgeInsets.zero,
+      constraints: BoxConstraints(),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+    );
+  }
+}
+
+class CaategoryList extends StatelessWidget {
+  const CaategoryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(width: 30, height: 30, color: Colors.amber),
+        SizedBox(width: 8),
+        Container(width: 30, height: 30, color: Colors.amber),
+        SizedBox(width: 8),
+        Container(width: 30, height: 30, color: Colors.amber),
+      ],
     );
   }
 }
